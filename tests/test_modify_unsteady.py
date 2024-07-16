a1e361144164ac34bbea41ef6fd91bb9f43ee1df
Ned Santiago <nedsantiago@tutanota.com>
2024-07-16 14:40:26 +0800

    Modified the tests. Inaccurate expected results. Included an inheritance system for extensible future testing

--- tests/test_modify_unsteady.py
+++ tests/test_modify_unsteady.py
@@ -6,50 +6,89 @@ import modify_unsteady
 import pytest
 
 
-def test_modify_unsteady01():
-    logger = logging.getLogger(__name__)
+class TestModifyUnsteady():
+    """
+    This class tests using an unsteady flow file that has complete lines of data. Example:
 
-    # declare input and expected output
-    input_path = r"tests\data\ras_model\input.u01"
-    output_path = r"tests\data\ras_model\output.u01"
-    dss_path = r".\DSS Files\MalateClogged10.dss"
-    
-    # save initial of input state to memory
-    init_state = None
-    with open(input_path, "r") as file:
-        rows = file.readlines()
-        init_state = rows.copy()
-
-    # run the script
+    Boundary Location=                ,                ,        ,        ,                ,P01             ,                ,0+225                           ,                                
+    Interval=1HOUR
+    Flow Hydrograph= 0 
+    Stage Hydrograph TW Check=0
+    DSS File=/IRREGULAR/TIMESERIES/FLOW/01Jan1990/IR-Decade/0+225/
+    DSS Path=.\\DSS Files\\example-dss.dss
+    Use DSS=True
+    Use Fixed Start Time=False
+    Fixed Start Date/Time=,
+    Is Critical Boundary=False
+    Critical Boundary Flow=
+    """
+    # declare inputs
+    input_path = r"tests\data\ras_model\input01.u01"
+    dss_path = r".\DSS Files\example-dss.dss"
     dss_base_internal_path = "/IRREGULAR/TIMESERIES/FLOW/01Jan1990/IR-Decade/"
-    ustd_processor = modify_unsteady.UnsteadyFlowFileProcessor()
-    ustd_processor = ustd_processor.dss_base_internal_path(dss_base_internal_path).dss_file_path(dss_path)
-    ustd_processor.run(input_path)
+    # declare expected output
+    output_path = r"tests\data\ras_model\output01.u01"
+    
+    def test_modify_unsteady(self):
+        logger = logging.getLogger(__name__)
+        # declare inputs
+        input_path = self.input_path
+        dss_path = self.dss_path
+        dss_base_internal_path = self.dss_base_internal_path
+        # declare expected output
+        output_path = self.output_path
 
-    # get results of input data
-    input_data = None
-    with open(input_path, "r") as file:
-        rows = file.readlines()
-        input_data = rows.copy()
+        # save input's initial state for reuse
+        init_state = None
+        with open(input_path, "r") as file:
+            rows = file.readlines()
+            init_state = rows.copy()
 
-    # write result to a temp.log file
-    with open(r"tests\temp.log", "w") as file:
-        file.writelines(input_data)
-
-    # get expected output results data
-    output_data = None
-    with open(output_path, "r") as file:
-        rows = file.readlines()
-        output_data = rows.copy()
-
-    # assert input and output are the same
-    err_msg = f"Input data and output data do not match, given:\nRESULTED:{input_data}\n---VS---\nEXPECTED:{output_data}"
-    if not input_data == output_data:
-        logger.error(err_msg)
-        raise AssertionError(err_msg)
+        # run the script
+        ustd_processor = modify_unsteady.UnsteadyFlowFileProcessor()
+        ustd_processor = ustd_processor.dss_base_internal_path(dss_base_internal_path).dss_file_path(dss_path)
         
+        try:
+            ustd_processor.run(input_path)
+        except Exception as e:
+            err_msg = f"Failed to complete unsteady flow file modification. {e}"
+            logger.error(err_msg)
+            # restore initial state
+            with open(input_path, "w") as file:
+                file.writelines(init_state)
+            raise RuntimeError(err_msg)
 
-    # restore initial state
-    with open(input_path, "w") as file:
-        file.writelines(init_state)
+        # get results of input data
+        input_data = None
+        with open(input_path, "r") as file:
+            rows = file.readlines()
+            input_data = rows.copy()
 
+        # get expected output results data
+        output_data = None
+        with open(output_path, "r") as file:
+            rows = file.readlines()
+            output_data = rows.copy()
+
+        # assert input and output are the same
+        err_msg = f"Input data and output data do not match, given:\nRESULTED:{input_data}\n---VS---\nEXPECTED:{output_data}"
+        if not input_data == output_data:
+            logger.error(err_msg)
+            raise AssertionError(err_msg)
+            
+        # restore initial state
+        with open(input_path, "w") as file:
+            file.writelines(init_state)
+    
+class TestModifyUnsteady01(TestModifyUnsteady):
+    """
+    This class tests using an unsteady flow file that has missing lines of data. Example:
+
+    Boundary Location=                ,                ,        ,        ,                ,Perimeter 1     ,                ,PCUNA052                        ,                                
+    """
+    # declare inputs
+    input_path = r"tests\data\ras_model\input.u01"
+    dss_path = r".\DSS Files\MalateClogged10.dss"
+    dss_base_internal_path = "/IRREGULAR/TIMESERIES/FLOW/01Jan1990/IR-Decade/"
+    # declare expected output
+    output_path = r"tests\data\ras_model\output.u01"
